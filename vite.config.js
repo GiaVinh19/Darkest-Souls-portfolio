678abd2f09d8d12aa2596edd7814c0eb3ab31997
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2024-08-26 15:51:21 -0400

    change back to default

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/darkest-souls-portfolio/", // keep this in mind
+  // base: "/darkest-souls-portfolio/", // keep this in mind
   server: {
     port: 3000, // Change this to your desired port number
   },
